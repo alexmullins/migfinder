74d143734cae6c5f94fa38fa0999e40a027fe0f6
Alex Mullins <jalexmullins91@gmail.com>
2020-01-05 02:33:41 -0600

    MigFinder works as expected

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
-from typing import List, Optional
-from binaryninja import BinaryView, PluginCommand
+from typing import List
+from collections import namedtuple
 
+from binaryninja import (BinaryReader, BinaryView, PluginCommand, Section,
+                         log_info)
+
+MIGSubsystem = namedtuple('MIGSubsystem', 'addr subsystem num_entries ')
 
 class MigFinder(object):
     """
@@ -11,27 +15,66 @@ class MigFinder(object):
 
     def __init__(self, bv: BinaryView):
         self._bv = bv
+        self._br = BinaryReader(bv)
 
-    def find() -> Optional[List[int]]:
+    def find(self) -> List[MIGSubsystem]:
         """
-        Find all of the possible MIG Handlers in the programs
+        Find all of the possible MIG Subsystems in the program's
         DATA and CONST sections.
-        Returns a list of address where each Mig Subsystem starts
-        or None if none are found.
+        Returns a list of MIGSubsystem.
         """
-        pass
+        bv = self._bv
+        found: List[MIGSubsystem] = list()
+
+        possible_sections: List[Section] = [
+            sec for n, sec in bv.sections.items() if "const" in n
+        ]
+        for section in possible_sections:
+            start = section.start
+            end = section.end
+            for addr in range(start, end, 8):
+                migsubsystem = self._is_valid(addr)
+                if migsubsystem is not None:
+                    found.append(migsubsystem)
+        return found
+
+    def _is_valid(self, addr) -> bool:
+        bv = self._bv
+        br = self._br
+        br.seek(addr)
+        # 1. check 1st 8-bytes is a pointer to a function
+        data = br.read64()
+        if bv.get_function_at(data) is None:
+            return None
+        # 2. read two 4-byte integers and ensure  first < second
+        first = br.read32()
+        second = br.read32()
+        if not first < second:
+            return None
+        # 3. read 4-byte maxsize field is > 0
+        maxsize = br.read32()
+        if not maxsize > 0:
+            return None
+        # 4. read 4-byte padding and then read 8-byte reserved and ensure == 0
+        padding = br.read32()
+        if not padding == 0:
+            log_info("padding is not 0")
+        reserved = br.read64()
+        if not reserved == 0:
+            return None
+        return MIGSubsystem(addr, first, second-first)
 
 
 class MigCreator(object):
     """
     Creates unique Mig subsystem structures based
-    on the MIG ID and number of handlers found.
+    on the MIG subsystem ID and number of handlers found.
     """
 
     def __init__(self, bv: BinaryView):
         self._bv = bv
 
-    def create(migs: List[int]):
+    def create(self, migs: List[int]):
         """
         Create Mig Subsytem structures at the addresses
         found in `migs`.
@@ -42,16 +85,14 @@ class MigCreator(object):
 def find_mig_handlers(bv: BinaryView):
     mf = MigFinder(bv)
     mc = MigCreator(bv)
-
     migs = mf.find()
-    if migs is None:
+    if len(migs) == 0:
         return
-
     mc.create(migs)
 
 
 def is_mig_valid(bv: BinaryView):
-    if "mac" not in bv.platform:
+    if "mac" not in bv.platform.name:
         return False
     if bv.get_symbols_by_name("_NDR_record") is None:
         return False
